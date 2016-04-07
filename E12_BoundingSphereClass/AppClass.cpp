81bb28367b4c1bcbb2226aa5575ee7c9fafb0a85
TAL COHEN (RIT Student) <tsc1510@ad.rit.edu>
2016-04-07 12:20:15 -0400

    Implemented in-class stuff.

--- E12_BoundingSphereClass/AppClass.cpp
+++ E12_BoundingSphereClass/AppClass.cpp
@@ -18,6 +18,103 @@ void AppClass::InitVariables(void)
 	//Load Models
 	m_pMeshMngr->LoadModel("Minecraft\\Steve.obj", "Steve");
 	m_pMeshMngr->LoadModel("Minecraft\\Creeper.obj", "Creeper");
+
+	std::vector<vector3> vertexList = m_pMeshMngr->GetVertexList("Steve");
+
+	vector3 v3Min, v3Max;
+	if (vertexList.size() > 0)
+	{
+		v3Min = vertexList.at(0);
+		v3Max = vertexList.at(0);
+	}
+
+	for (uint i = 0; i < vertexList.size(); i++)
+	{
+		vector3 vertex = vertexList.at(i);
+		if (vertex.x < v3Min.x)
+		{
+			v3Min.x = vertex.x;
+		}
+		else if (vertex.x > v3Max.x)
+		{
+			v3Max.x = vertex.x;
+		}
+
+		if (vertex.y < v3Min.y)
+		{
+			v3Min.y = vertex.y;
+		}
+		else if (vertex.y > v3Max.y)
+		{
+			v3Max.y = vertex.y;
+		}
+
+		if (vertex.z < v3Min.z)
+		{
+			v3Min.z = vertex.z;
+		}
+		else if (vertex.z > v3Max.z)
+		{
+			v3Max.z = vertex.z;
+		}
+	}
+
+	m_v3Center1 = (v3Max + v3Min) / 2.0f;
+	m_fRadius1 = glm::distance(m_v3Center1, v3Min);
+
+	m_pSphere1 = new PrimitiveClass();
+	m_pSphere1->GenerateSphere(m_fRadius1, 10, REGREEN);
+
+
+	vertexList.clear();
+	vertexList = m_pMeshMngr->GetVertexList("Creeper");
+
+	v3Min = vector3(0.0f);
+	v3Max = vector3(0.0f);
+
+	// CREEPER
+	if (vertexList.size() > 0)
+	{
+		v3Min = vertexList.at(0);
+		v3Max = vertexList.at(0);
+	}
+
+	for (uint i = 0; i < vertexList.size(); i++)
+	{
+		vector3 vertex = vertexList.at(i);
+		if (vertex.x < v3Min.x)
+		{
+			v3Min.x = vertex.x;
+		}
+		else if (vertex.x > v3Max.x)
+		{
+			v3Max.x = vertex.x;
+		}
+
+		if (vertex.y < v3Min.y)
+		{
+			v3Min.y = vertex.y;
+		}
+		else if (vertex.y > v3Max.y)
+		{
+			v3Max.y = vertex.y;
+		}
+
+		if (vertex.z < v3Min.z)
+		{
+			v3Min.z = vertex.z;
+		}
+		else if (vertex.z > v3Max.z)
+		{
+			v3Max.z = vertex.z;
+		}
+	}
+
+	m_v3Center2 = (v3Max + v3Min) / 2.0f;
+	m_fRadius2 = glm::distance(m_v3Center2, v3Min);
+
+	m_pSphere2 = new PrimitiveClass();
+	m_pSphere2->GenerateSphere(m_fRadius2, 10, REGREEN);
 }
 
 void AppClass::Update(void)
@@ -43,6 +140,12 @@ void AppClass::Update(void)
 
 	//Indicate the FPS
 	int nFPS = m_pSystem->GetFPS();
+
+	bool areColliding = false;
+	m_pMeshMngr->Print("x: " + std::to_string(m_v3Center1.x) + " ", RERED);
+	m_pMeshMngr->Print("y: " + std::to_string(m_v3Center1.y) + " ", RERED);
+	m_pMeshMngr->Print("z: " + std::to_string(m_v3Center1.z) + " ", RERED);
+
 	//print info into the console
 	printf("FPS: %d            \r", nFPS);//print the Frames per Second
 	//Print info on the screen
@@ -72,6 +175,20 @@ void AppClass::Display(void)
 		m_pMeshMngr->AddGridToQueue(1.0f, REAXIS::XY, REBLUE * 0.75f); //renders the XY grid with a 100% scale
 		break;
 	}
+
+	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
+	matrix4 m4Model = m_pMeshMngr->GetModelMatrix("Steve") * glm::translate(m_v3Center1);
+
+	m_pSphere1->Render(m4Projection, m4View, m4Model);
+
+
+
+	m4Projection = m_pCameraMngr->GetProjectionMatrix();
+	m4View = m_pCameraMngr->GetViewMatrix();
+	m4Model = m_pMeshMngr->GetModelMatrix("Creeper") * glm::translate(m_v3Center2);
+
+	m_pSphere1->Render(m4Projection, m4View, m4Model);
 	
 	m_pMeshMngr->Render(); //renders the render list
 
@@ -81,4 +198,16 @@ void AppClass::Display(void)
 void AppClass::Release(void)
 {
 	super::Release(); //release the memory of the inherited fields
+
+	if (m_pSphere1 != nullptr)
+	{
+		delete m_pSphere1;
+		m_pSphere1 = nullptr;
+	}
+
+	if (m_pSphere2 != nullptr)
+	{
+		delete m_pSphere2;
+		m_pSphere2 = nullptr;
+	}
 }
--- E12_BoundingSphereClass/AppClass.h
+++ E12_BoundingSphereClass/AppClass.h
@@ -15,6 +15,16 @@ class AppClass : public ReEngAppClass
 {
 	vector3 m_v3O1 = vector3(-2.5f, 0.0f, 0.0f);
 	vector3 m_v3O2 = vector3( 2.5f, 0.0f, 0.0f);
+
+	PrimitiveClass* m_pSphere1;
+	vector3 m_v3Center1;
+	float m_fRadius1 = 0;
+
+
+	PrimitiveClass* m_pSphere2;
+	vector3 m_v3Center2;
+	float m_fRadius2 = 0;
+
 public:
 	typedef ReEngAppClass super;
 	/*
